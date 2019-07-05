6d12b00e076ec3b433e01bd3a9a874e6a2efc652
Valentyna Sliuniaieva <val.slyun@gmail.com>
2019-07-05 18:08:50 +0300

    Add validations (#5)
     improved method max_length

--- spec/support/users/validators/user_validator.rb
+++ spec/support/users/validators/user_validator.rb
@@ -3,5 +3,6 @@ module FrontendApi
     validates :presence, %i[name]
     validates :uniqueness, %i[name]
     validates :positive, :position, Integer, if: -> { @user.position }
+    validates :max_length, :name, 16, if: -> {@user.name}
   end
 end
